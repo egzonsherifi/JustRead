e3dbf175ab1fe95d006ebbb4850adbbdbf5d965e
xonni1 <91503950+egzonsherifi@users.noreply.github.com>
2021-11-09 00:04:22 +0100

    Registration form added

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,79 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10 bg-yellow-100 border border-gray-400 p-6 rounded-3xl">
+            <h1 class="text-center font-bold text-xl">Register</h1>
+            <form method="POST" action="/register" class="mt-10">
+                @csrf
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="name"
+                    >
+                        Name
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="text"
+                           name="name"
+                           id="name"
+                           required
+                    >
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="username"
+                    >
+                        Username
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="text"
+                           name="username"
+                           id="username"
+                           required
+                    >
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="email"
+                    >
+                        Email
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="email"
+                           name="email"
+                           id="email"
+                           required
+                    >
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="password"
+                    >
+                        Password
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="password"
+                           name="password"
+                           id="password"
+                           required
+                    >
+                </div>
+
+                    <div class="mb-6">
+                      <button
+                      class="text-gray-400 bg-transparent border border-solid border-gray-400 hover:bg-gray-500 hover:text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="submit">
+                      Submit
+                    </button>
+
+                      </div>
+                  </div>
+            </form>
+        </main>
+    </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BookController::class, 'index'])->name('home');
 Route::get('books/{book:slug}', [BookController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
