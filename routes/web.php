8c823483c9fb20feb0555f9efcdbbc3b688cad1c
xonni1 <91503950+egzonsherifi@users.noreply.github.com>
2021-11-16 23:23:45 +0100

    Newsletter service extracted

--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,15 @@
 
 use App\Http\Controllers\BookCommentsController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::post('newsletter', function () {
-    request()->validate([
-        'email' => 'required|email'
-    ]);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us5'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->AddListMember('e63393ab9a', [
-            'email_address' => request('email'),
-            'status' => 'subscribed'
-        ]);
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    }
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-});
-
 Route::get('/', [BookController::class, 'index'])->name('home');
 Route::get('books/{book:slug}', [BookController::class, 'show']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::post('books/{book:slug}/comments', [BookCommentsController::class, 'store']);
 
