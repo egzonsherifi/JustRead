6f64929b4b669187a29b84d1f23b401ec653e3aa
xonni1 <91503950+egzonsherifi@users.noreply.github.com>
2021-11-18 21:14:31 +0100

    Few things updated

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -4,91 +4,12 @@
             <h1 class="text-center font-bold text-xl">Register</h1>
             <form method="POST" action="/register" class="mt-10">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="name"
-                    >
-                        Name
-                    </label>
+                <x-form.input name="name" />
+                <x-form.input name="username" />
+                <x-form.input name="email" type="email" autocomplete="username" />
+                <x-form.input name="password" type="password" autocomplete="new-password" />
 
-                    <input class="border border-gray-400 p-2 w-full bg-yellow-100 rounded"
-                           type="text"
-                           name="name"
-                           id="name"
-                           value="{{ old('name') }}"
-                           required
-                    >
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1"  >{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="username"
-                    >
-                        Username
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full bg-yellow-100 rounded"
-                           type="text"
-                           name="username"
-                           id="username"
-                           value="{{ old('username') }}"
-                           required
-                    >
-                    @error('username')
-                    <p class="text-red-500 text-xs mt-1"  >{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="email"
-                    >
-                        Email
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full bg-yellow-100 rounded"
-                           type="email"
-                           name="email"
-                           id="email"
-                           value="{{ old('email') }}"
-                           required
-                    >
-                    @error('email')
-                    <p class="text-red-500 text-xs mt-1"  >{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="password"
-                    >
-                        Password
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full bg-yellow-100 rounded"
-                           type="password"
-                           name="password"
-                           id="password"
-
-                           required
-                    >
-                    @error('password')
-                    <p class="text-red-500 text-xs mt-1"  >{{ $message }}</p>
-                    @enderror
-                </div>
-
-                    <div class="mb-6">
-                      <button
-                      class="text-gray-400 bg-transparent border border-solid border-gray-400 hover:bg-gray-500 hover:text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="submit">
-                      Submit
-                    </button>
-
-                      </div>
-                  </div>
+                <x-form.button>Submit</x-form.button>
             </form>
         </main>
     </section>
